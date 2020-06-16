5f6dbc806ae6169568a0b86d2c2d63df7292c967
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-06-16 10:47:42 +0700

    Added pages files

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.get('/', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'pages/index.html'));
 });
 
+app.get('/index.html', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about.html', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+app.get('/contact.html', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+
+app.get('/post.html', (req, res)  => {
+	res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+});
+
 app.listen(3000, () => {
 	console.log("App listening on port 3000");
 });
