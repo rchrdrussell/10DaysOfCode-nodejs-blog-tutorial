d995c280d3c53168b8d5a17acea62ffd2b4058b1
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-06-16 11:05:51 +0700

    Added Edge templating engine to layout pages

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const path = require('path');
 //Import Express modules
 const express = require('express');
+const expressEdge = require('express-edge');
+const path = require('path');
 
 //Initialize the express app
 const app = new express();
 
 //Use the modules
 app.use(express.static('public')); //Uses express in a static web form
+app.use(expressEdge.engine);
+app.set('views', __dirname + '/views');
 
+//Route methods
 app.get('/', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+	res.render('index');
 });
 
 app.get('/index.html', (req, res) => {
