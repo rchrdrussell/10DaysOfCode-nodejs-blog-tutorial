9530ce29c7161ccbb7bbff07815994d096c61237
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-06-16 10:36:28 +0700

    Added changes to home page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 //Import Express modules
 const express = require('express');
 
@@ -6,6 +7,10 @@ const app = new express();
 
 //Use the modules
 app.use(express.static('public')); //Uses express in a static web form
+
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
 
 app.listen(3000, () => {
 	console.log("App listening on port 3000");
